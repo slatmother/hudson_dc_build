428c74f7e9f62fc20916c27960c89454d38b1c0e
ggstar <slatmother@gmail.com>
2013-08-09 18:54:21 +0400

    commit

---
+++ TestNIO.java
@@ -0,0 +1,52 @@
+/*
+* $Id
+*
+* (C) Copyright 1997 i-Teco, CJSK. All Rights reserved.
+* i-Teco PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+*
+* Эксклюзивные права 1997 i-Teco, ЗАО.
+* Данные исходные коды не могут использоваться и быть изменены
+* без официального разрешения компании i-Teco.          
+*/
+
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+
+/**
+ * $Id
+ * <p>Title: </p>
+ * <p>Description: </p>
+ * <p>Author: g.alexeev (g.alexeev@i-teco.ru)</p>
+ * <p>Date: 06.08.13</p>
+ *
+ * @version 1.0
+ */
+public class TestNIO {
+    public static void main(String[] args) {
+        try {
+            Path startPath = Paths.get("D:\\SVNProjects\\hudson_dc_build");
+            Files.walkFileTree(startPath, new SimpleFileVisitor<Path>() {
+                @Override
+                public FileVisitResult preVisitDirectory(Path dir,
+                                                         BasicFileAttributes attrs) {
+                    System.out.println("Dir: " + dir.toString());
+                    return FileVisitResult.CONTINUE;
+                }
+
+                @Override
+                public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) {
+                    System.out.println("File: " + file.toString());
+                    return FileVisitResult.CONTINUE;
+                }
+
+                @Override
+                public FileVisitResult visitFileFailed(Path file, IOException e) {
+                    return FileVisitResult.CONTINUE;
+                }
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
